7774f3d12a3dc04643fee30ab39ba7b15dfb2eb0
jesse <jesse@gmail.com>
2024-11-05 19:28:18 +0530

    first commit

---
+++ controllers/authControllers.js
@@ -0,0 +1,54 @@
+const adminModel = require("../models/adminModel");
+const { responseReturn } = require("../utilities/response");
+const bcrypt = require("bcrypt");
+const { createToken } = require("../utilities/tokenCreate");
+
+class authControllers {
+  admin_login = async (req, res) => {
+    console.log(req.body);
+    const { email, password } = req.body;
+    try {
+      const admin = await adminModel.findOne({ email }).select("+password");
+      // console.log(admin);
+      if (admin) {
+        const match = await bcrypt.compare(password, admin.password);
+        // console.log(match);
+        if (match) {
+          const token = await createToken({
+            id: admin.id,
+            role: admin.role,
+          });
+          res.cookie("accessToken", token, {
+            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+          });
+          responseReturn(res, 200, { token, message: "Login is successful" });
+        } else {
+          responseReturn(res, 404, { error: "Password is incorrect" });
+        }
+      } else {
+        responseReturn(res, 404, { error: "Email not found" });
+      }
+    } catch (error) {
+      responseReturn(res, 500, { error: error.message });
+    }
+  };
+
+  //
+
+  getUser = async (req, res) => {
+    const { id, role } = req;
+
+    try {
+      if (role === "admin") {
+        const user = await adminModel.findById(id);
+        responseReturn(res, 200, { userInfo: user });
+      } else {
+        console.log("Seller Info");
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+}
+
+module.exports = new authControllers();
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+const port = process.env.PORT;
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const { dbConnect } = require("./utilities/db");
+
+dbConnect();
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use("/api", require("./routes/authRoutes"));
+
+app.get("/", (req, res) => res.send("My Backend"));
+
+app.listen(port, () => console.log(`server running  on ${port}`));
