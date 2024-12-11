23bffc0ee41f1e80dc881a573425963e1b8944e4
jesse <jesse@gmail.com>
2024-12-11 19:56:59 +0530

    firstnjb4

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,4 +1,6 @@
 const adminModel = require("../models/adminModel");
+const sellerModel = require("../models/sellerModel");
+const sellerCustomerModel = require("../models/chat/sellerCustomerModel");
 const { responseReturn } = require("../utilities/response");
 const bcrypt = require("bcrypt");
 const { createToken } = require("../utilities/tokenCreate");
@@ -35,6 +37,65 @@ class authControllers {
 
   //
 
+  seller_register = async (req, res) => {
+    const { email, name, password } = req.body;
+    try {
+      const getUser = await sellerModel.findOne({ email });
+      if (getUser) {
+        responseReturn(res, 404, { error: "Email already exists" });
+      } else {
+        const seller = await sellerModel.create({
+          name,
+          email,
+          password: await bcrypt.hash(password, 10),
+          method: "manually",
+          shopInfo: {},
+        });
+        await sellerCustomerModel.create({
+          myId: seller.id,
+        });
+        const token = await createToken({
+          id: seller.id,
+          role: seller.role,
+        });
+        res.cookie("acessToken", token, {
+          expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        });
+        responseReturn(res, 201, { token, message: "Register Success" });
+      }
+    } catch (error) {
+      responseReturn(res, 500, { error: " Internal server error" });
+    }
+  };
+
+  seller_login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      const seller = await sellerModel.findOne({ email }).select("+password");
+      // console.log(admin);
+      if (seller) {
+        const match = await bcrypt.compare(password, seller.password);
+        // console.log(match);
+        if (match) {
+          const token = await createToken({
+            id: seller.id,
+            role: seller.role,
+          });
+          res.cookie("accessToken", token, {
+            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+          });
+          responseReturn(res, 200, { token, message: "Login is successful" });
+        } else {
+          responseReturn(res, 404, { error: "Password is incorrect" });
+        }
+      } else {
+        responseReturn(res, 404, { error: "Email not found" });
+      }
+    } catch (error) {
+      responseReturn(res, 500, { error: error.message });
+    }
+  };
+
   getUser = async (req, res) => {
     const { id, role } = req;
 
@@ -43,10 +104,11 @@ class authControllers {
         const user = await adminModel.findById(id);
         responseReturn(res, 200, { userInfo: user });
       } else {
-        console.log("Seller Info");
+        const user = await sellerModel.findById(id);
+        responseReturn(res, 200, { userInfo: user });
       }
     } catch (error) {
-      console.log(error.message);
+      responseReturn(res, 500, { error: "Internal Server Error" });
     }
   };
 }
